956f5e08bc49fee6fe9c068318147a3524be2251
lishen2 <lishen2@lenovo.com>
2020-03-09 20:19:52 +0800

    配置.gitignore文件忽略所有的node_modules文件夹

---
+++ vue-webpack/babel/compiled.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// const app = ()=>{
+//     alert('1234567')
+// }
+
+var li = document.querySelectorAll('li');
+
+// (Hoisting) javascript中的变量提升
+
+// for(let i=0; i<li.length; i++){
+//     li[i].onclick = ()=>{
+//         console.log(i)
+//     }
+// }
+
+// for(var i=0; i<li.length; i++){
+//     ((i)=>{ // 自执行函数
+//         li[i].onclick = ()=>{
+//             console.log(i)
+//         }
+//     })(i)
+// }
+
+// 引用类型使用const定义的数组或者对象可以修改内容，但是不允许改变其类型。
+// 引用类型的赋值不能用等号
+
+
+// // babel会将es6的反引号 `` 里的中文转为unicode编码   
+// const name = "abc";
+// const msg = "你是个";
+var str = "\u5927\u50BB\u86CB aa 123" + "";
+
+console.log(str);
---
+++ vue-webpack/babel/es7.js
@@ -0,0 +1,3 @@
+let mike = { name: "mike", age: "40" }
+mike = {...mike, sex: "male"}
+console.log(mike)
